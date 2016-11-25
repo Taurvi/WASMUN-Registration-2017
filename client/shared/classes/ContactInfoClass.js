0fa7f88838dcecdbdf190796a275f39b45bf28c9
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2016-11-24 16:09:43 -0800

    Began writing ContactInfoClass and associated tests.

--- client/shared/classes/ContactInfoClass.js
+++ client/shared/classes/ContactInfoClass.js
@@ -2,6 +2,27 @@
 var RegistrationModule = angular.module('RegistrationModule')
 RegistrationModule.factory('ContactInfoClass', [function () {
     function ContactInfoClass() {
+        var self = this;
+        self._name = 'fooBar';
+    }
+
+    Object.defineProperties(ContactInfoClass.prototype, {
+        'name': {
+            get: function () {
+                return this._name;
+            },
+            set: function(newName) {
+                this._name = newName;
+            }
+        }
+    });
+
+    ContactInfoClass.prototype.getName = function() {
+        return this.name;
+    }
+
+    ContactInfoClass.prototype.setName = function(newName) {
+        this.name = newName;
     }
 
     return ContactInfoClass;
--- client/shared/classes/ContactInfoClass.test.js
+++ client/shared/classes/ContactInfoClass.test.js
@@ -5,10 +5,26 @@ describe('ContactInfoClass Tests', function(){
 
     // Set our injected Users factory (_Users_) to our local Users variable
     beforeEach(inject(function(_ContactInfoClass_) {
-        ContactInfoClass = _ContactInfoClass_;
+        ContactInfoClass = new _ContactInfoClass_;
     }));
 
-    it('ContactInfoClass should be defined', function() {
+    it('Class should be defined', function() {
         expect(ContactInfoClass).toBeDefined();
     });
+
+    describe('Class name tests', function() {
+        it('Class should have getName be defined', function() {
+            expect(ContactInfoClass.getName()).toBeDefined();
+        });
+
+        it('Class should get name value "fooBar"', function() {
+            expect(ContactInfoClass.getName()).toBe('fooBar');
+        });
+
+        it('Class should setName to barFoo"', function() {
+            expect(ContactInfoClass.setName('barFoo'));
+            expect(ContactInfoClass.getName()).toBe('barFoo');
+        });
+    })
 });
+
