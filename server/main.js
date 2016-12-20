9bdea83fedd0f2fc29962709fe04aa264c0bb48d
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2016-12-20 11:17:08 -0800

    Adjusted CORS?

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(server, { origins: '*:*'});
+var io = require('socket.io')(http, { origins: '*:*'});
 
 var Matrix = require('./matrix/matrix').Matrix;
 var matrix = new Matrix();
